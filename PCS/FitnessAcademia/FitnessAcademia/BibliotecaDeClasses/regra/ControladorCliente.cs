dcf0c76f670a5ea958f553ba5f5da2dbb68aa504
alcidelio@gmail.com <alcidelio@gmail.com@85bf08c8-2cd5-c4b4-259b-036b397dac1f>
2013-05-14 16:07:51 +0000

    ControladorCliente parcialmente feito

--- PCS/FitnessAcademia/FitnessAcademia/BibliotecaDeClasses/regra/ControladorCliente.cs
+++ PCS/FitnessAcademia/FitnessAcademia/BibliotecaDeClasses/regra/ControladorCliente.cs
@@ -10,6 +10,77 @@ namespace BibliotecaDeClasses.regra
 {
     public class ControladorCliente
     {
+        private IRepositorioCliente rpC;
 
+        public ControladorCliente()
+        {
+            rpC = new RepositorioCliente();
+        }
+
+        public bool existe(Cliente c)
+        {
+            List<Cliente> lista = rpC.consultar(c);
+            if (lista.Count > 0)
+                return true;
+            return false;
+        }
+        public void validarDados(Cliente c)
+        {
+            if (c.Nome == null)
+                throw new ErroValidacao("Erro ao validar cliente!");
+            if (c.Nome.Length == 0 || c.Nome.Length > 100)
+                throw new ErroValidacao("Erro ao validar cliente");
+
+            //------------validar CPF--------------------------
+
+            //------------validar RG--------------------------
+
+            //------------validar data de nascimento-----------
+
+            //if (c.Logradouro == null)
+               // throw new ErroValidacao("Erro ao validar Logradouro!");
+            if (c.Logradouro.Length == 0 || c.Logradouro.Length > 100)
+                throw new ErroValidacao("Erro ao validar Logradouro");
+
+            //if (c.NumLog == null)
+               // throw new ErroValidacao("Erro ao validar NumLog!");
+            if (c.NumLog.Length == 0 || c.NumLog.Length > 15)
+                throw new ErroValidacao("Erro ao validar NumLog");
+
+           // if (c.Complemento == null)
+               // throw new ErroValidacao("Erro ao validar Complemento!");
+            if (c.Complemento.Length == 0 || c.Complemento.Length > 50)
+                throw new ErroValidacao("Erro ao validar Complemento");
+
+            if (c.Bairro.Length == 0 || c.Bairro.Length > 60)
+                throw new ErroValidacao("Erro ao validar Complemento");
+
+            if (c.Cidade.Length == 0 || c.Cidade.Length > 60)
+                throw new ErroValidacao("Erro ao validar Complemento");
+
+            if (c.Uf.Length == 0 || c.Uf.Length > 2)
+                throw new ErroValidacao("Erro ao validar Complemento");
+
+            if (c.Cep.Length == 0 || c.Cep.Length > 8)
+                throw new ErroValidacao("Erro ao validar Complemento");
+
+            if (c.EstCivil.Length == 0 || c.EstCivil.Length > 1)
+                throw new ErroValidacao("Erro ao validar Complemento");
+
+            if (c.Sexo.Length == 0 || c.Sexo.Length > 1)
+                throw new ErroValidacao("Erro ao validar Complemento");
+
+            if (c.Telefone.Length == 0 || c.Telefone.Length > 11)
+                throw new ErroValidacao("Erro ao validar Complemento");
+
+            if (c.Celular.Length == 0 || c.Celular.Length > 11)
+                throw new ErroValidacao("Erro ao validar Complemento");
+
+            if (c.Email.Length == 0 || c.Email.Length > 100)
+                throw new ErroValidacao("Erro ao validar Complemento");
+
+           //----------------Validade exame médico-------------------
+
+        }
     }
 }
