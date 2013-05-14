d921e08cad8aaa16eac85621b0596993ee9ec31b
alcidelio@gmail.com <alcidelio@gmail.com@85bf08c8-2cd5-c4b4-259b-036b397dac1f>
2013-05-14 14:49:15 +0000

    ControladorCliente adiconada por alcidelio em 14/05/2013

---
+++ PCS/FitnessAcademia/FitnessAcademia/BibliotecaDeClasses/regra/ControladorCliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BibliotecaDeClasses.basica;
+using BibliotecaDeClasses.dao;
+using BibliotecaDeClasses.erro;
+
+namespace BibliotecaDeClasses.regra
+{
+    public class ControladorCliente
+    {
+
+    }
+}
