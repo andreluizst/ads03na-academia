dce61b0277f577d945acfdbe4ee37ccb99897605
andre.luiz.silvat@gmail.com <andre.luiz.silvat@gmail.com@85bf08c8-2cd5-c4b4-259b-036b397dac1f>
2013-05-14 00:53:51 +0000

    Atualização: fachada/Fachada.cs e ErroValidacao.

---
+++ PCS/FitnessAcademia/FitnessAcademia/BibliotecaDeClasses/erro/ErroValidacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BibliotecaDeClasses.erro
+{
+    class ErroValidacao : Exception
+    {
+        public ErroValidacao()
+            : base()
+        {
+        }
+
+        public ErroValidacao(String message)
+            : base(message)
+        {
+        }
+
+        public ErroValidacao(String message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
