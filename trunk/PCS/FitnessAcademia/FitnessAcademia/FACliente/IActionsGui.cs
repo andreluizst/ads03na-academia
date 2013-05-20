d472e4154c3108905dc15f9c15c6f16b4c657a1d
andre.luiz.silvat@gmail.com <andre.luiz.silvat@gmail.com@85bf08c8-2cd5-c4b4-259b-036b397dac1f>
2013-05-20 02:32:41 +0000

    adicionando IActionsGui ao repositório.

---
+++ trunk/PCS/FitnessAcademia/FitnessAcademia/FACliente/IActionsGui.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FACliente
+{
+    public interface IActionsGui
+    {
+        void novo();
+        void alterar();
+        void excluir();
+        void pesquisar();
+        void fecharPesquisa();
+        bool pesquisarExiste();
+        void setOpenMenuShell(ToolStripMenuItem miOpenGui);
+        //string getActivationName();
+    }
+}
